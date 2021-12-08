50dcd29e06ebf6e70458d767f1951e4049970280
easy297 <93608846+easy297@users.noreply.github.com>
2021-12-09 02:12:10 +0600

    insertion sort

--- insertionSort/main.cpp
+++ insertionSort/main.cpp
@@ -6,9 +6,40 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+        for (i = 1; i < n; i++)
+        {
+            key = arr[i];
+            j = i - 1;
+     
+            /* Move elements of arr[0..i-1], that are
+            greater than key, to one position ahead
+            of their current position */
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = 5;
+    insertionSort(arr, n);
+    printArray(arr, n);
     return 0;
 }
